55a6dc890c08373998493037bd34e9e352be9c75
Jiri Lhotka <jl887@cantab.ac.uk>
2022-05-16 09:56:05 +0200

    Add batchsize back

--- Miniproject2/model.py
+++ Miniproject2/model.py
@@ -257,17 +257,19 @@ class NNupsampling():
 class MSE(Module):
     def __init__(self):
         super()
+        self.batch_size = None
         self.last_input_diff = None
 
 
     def forward(self, *input):
         assert len(input) == 2
         assert len(input[0]) == len(input[1])
+        self.batch_size = input[0].shape[0]
         self.last_input_diff = (input[1] - input[0])
         return (self.last_input_diff**2).mean()
 
     def backward(self, *gradwrtoutput):
-        preliminary_loss =  -2*self.last_input_diff.mean(dim=1)
+        preliminary_loss =  ( - 2/self.batch_size)*self.last_input_diff
         if len(gradwrtoutput) != 1:
             return preliminary_loss
         return gradwrtoutput[0]*preliminary_loss
