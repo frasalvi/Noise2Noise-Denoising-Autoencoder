f1ed3cb3511b5994fd9f841f5a77b0f9d0370690
Lukas van den Heuvel <lukasvandenheuvel@tsf-492-wpa-5-229.epfl.ch>
2022-05-16 09:28:13 +0200

    Update Conv2D and add NN upsampling

--- Miniproject2/model.py
+++ Miniproject2/model.py
@@ -5,6 +5,8 @@ from functools import reduce
 
 set_grad_enabled(False)
 
+def floor(x):
+  return int(x//1)
 
 class Module(object):
     '''
@@ -79,6 +81,7 @@ class Linear(Module):
         return [(self.weight, self.weight.grad),
                 (self.bias, self.bias.grad)]
 
+'''
 
 class Conv2d(Module):
 
@@ -131,7 +134,60 @@ class Conv2d(Module):
         self.bias.grad = gradwrtoutput_unfolded.sum(axis=(0,2)).view(self.bias.shape)
         gradwrtinput_unfolded = (kernel.transpose(0,1) @ gradwrtoutput_unfolded)
         return fold(gradwrtinput_unfolded, output_size=self.input.shape[2:4], kernel_size=self.kernel_size)
+'''
 
+class Conv2d():
+  
+  def  __init__(self, in_channels, out_channels, kernel_size, 
+                stride=1, padding=0, dilation=1):
+    # Implements 2D convolution.
+
+    # Check if kernel_size is correct
+    if type(kernel_size)==int:
+      kernel_size = (kernel_size,kernel_size)
+    elif (type(kernel_size)==tuple and len(kernel_size)==2):
+      pass
+    else:
+      raise ValueError('Invalid dimensions of kernel_size. It should be either an integer or a tuple of length 2.')
+   
+    self.in_channels = in_channels
+    self.out_channels = out_channels
+    self.kernel_size = kernel_size
+    self.padding = padding
+    self.dilation = dilation
+    self.stride = stride
+
+    # Weight initialization. Replace this with sth. more sophisticated later
+    self.weight = empty(out_channels,in_channels,kernel_size[0],kernel_size[1]).normal_()
+    self.bias = empty(out_channels).normal_()
+  
+  def forward(self, *input):
+    
+    # Get shapes
+    self.input = input[0]
+    self.batch_size = self.input.shape[0]
+    # Output shape (in 1D) = floor((H + 2P - D*(K-1) - 1)/S + 1)
+    outH = floor((self.input.shape[2] + 2*self.padding - self.dilation * (self.kernel_size[0] - 1) -1 ) / self.stride + 1)
+    outW = floor((self.input.shape[3] + 2*self.padding - self.dilation * (self.kernel_size[1] - 1) -1 ) / self.stride + 1)
+    self.output_shape = (self.batch_size, self.out_channels, outH, outW)
+
+    # Do convolution as matrix-matrix product
+    unfolded = unfold(self.input, kernel_size=self.kernel_size, dilation=self.dilation, padding=self.padding, stride=self.stride)
+    convolved = self.weight.view(self.out_channels, -1).unsqueeze(0) @ unfolded + self.bias.view(1, -1, 1).unsqueeze(0)    
+    return convolved.view(self.output_shape)
+
+  def backward(self, *gradwrtoutput):
+    
+    # Get the unfolded versions of the input, gradient w.r.t. output and kernel
+    input_unfolded = unfold(self.input, kernel_size=self.kernel_size, dilation=self.dilation, padding=self.padding, stride=self.stride)
+    gradwrtoutput_unfolded = gradwrtoutput[0].view(self.batch_size, self.out_channels, self.output_shape[2]*self.output_shape[3])
+    kernel = self.weight.view(self.out_channels, -1)
+    
+    # Calculate weight and bias updates
+    self.weight.grad = (gradwrtoutput_unfolded @ input_unfolded.transpose(1,2)).sum(axis=0).view(self.weight.shape)
+    self.bias.grad = gradwrtoutput_unfolded.sum(axis=(0,2)).view(self.bias.shape)
+    gradwrtinput_unfolded = (kernel.transpose(0,1) @ gradwrtoutput_unfolded)
+    return fold(gradwrtinput_unfolded, output_size=self.input.shape[2:4], kernel_size=self.kernel_size, dilation=self.dilation, padding=self.padding, stride=self.stride)
 
 class ReLU(Module):
     def __init__(self):
@@ -182,3 +238,22 @@ class Sequential(Module):
         for layer in self.layers[::-1]:
             x = layer.backward(x)
         return x
+    
+    class NNupsampling():
+        def  __init__(self, scale_factor=2):
+            self.scale_factor = scale_factor
+    
+        def forward(self, *input):
+            # The function repeat_interleave(num_repeats, dim) repeats elements in a matrix
+            # num_repeats times along the specified dimension.
+            self.input = input[0]
+            out = input[0].repeat_interleave(repeats=self.scale_factor, dim=3).repeat_interleave(repeats=self.scale_factor, dim=2)
+            return out
+      
+        def backward(self, *gradwrtoutput):
+            # The gradient of a NNupsampling layer is the sum of the upsampled elements in gradwrtoutput.
+            # Example: if scale_factor=2, the gradient is the sum of 2x2 areas in gradwrtoutput.
+            # First unfold the matrix to easily take the sum
+            unfolded = unfold(gradwrtoutput[0], kernel_size=self.upsampling_factor, stride=self.upsampling_factor)
+            summed = unfolded.view(self.input.shape[0], self.input.shape[1], self.upsampling_factor**2, unfolded.shape[-1]).sum(dim=2)
+            return summed.view(self.input.shape)
