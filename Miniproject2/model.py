e2c18d0ba6c916a15e98625968876cface53a11d
Jiri Lhotka <jl887@cantab.ac.uk>
2022-05-16 09:54:03 +0200

    Swap order and keep dimension

--- Miniproject2/model.py
+++ Miniproject2/model.py
@@ -263,11 +263,11 @@ class MSE(Module):
     def forward(self, *input):
         assert len(input) == 2
         assert len(input[0]) == len(input[1])
-        self.last_input_diff = (input[0] - input[1])
+        self.last_input_diff = (input[1] - input[0])
         return (self.last_input_diff**2).mean()
 
     def backward(self, *gradwrtoutput):
-        preliminary_loss =  -2*self.last_input_diff.mean()
+        preliminary_loss =  -2*self.last_input_diff.mean(dim=1)
         if len(gradwrtoutput) != 1:
             return preliminary_loss
         return gradwrtoutput[0]*preliminary_loss
