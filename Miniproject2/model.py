56a36bf9933187d7dde52d803c99c0e0bcbaaf3b
Jiri Lhotka <jl887@cantab.ac.uk>
2022-05-16 09:52:26 +0200

    Use mean in MSE backprop also

--- Miniproject2/model.py
+++ Miniproject2/model.py
@@ -257,19 +257,17 @@ class NNupsampling():
 class MSE(Module):
     def __init__(self):
         super()
-        self.last_input_lenght = None
         self.last_input_diff = None
 
 
     def forward(self, *input):
         assert len(input) == 2
         assert len(input[0]) == len(input[1])
-        self.last_input_lenght = len(input[0])
         self.last_input_diff = (input[0] - input[1])
         return (self.last_input_diff**2).mean()
 
     def backward(self, *gradwrtoutput):
-        preliminary_loss =  ( - 2/self.last_input_lenght)*self.last_input_diff
+        preliminary_loss =  -2*self.last_input_diff.mean()
         if len(gradwrtoutput) != 1:
             return preliminary_loss
         return gradwrtoutput[0]*preliminary_loss
